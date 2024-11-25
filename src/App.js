f3a1f67b6dbf3d9a2bb0bf89b8c2e076f5d971c8
coffeeSR <shaktirajbhandari@gmail.com>
2024-11-25 15:52:39 +0545

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import { useEffect, useState } from "react";
 import Wordle from './components/Wordle'
 
 function App() {
+  // const [solution , setSolution] = useState(null);
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/solutions')
+  //   .then(res => res.json())
+  //   .then(json => {
+  //     //random int between 0 and 24
+  //       const  randomSolution = json[Math.floor(Math.random()*json.length)]
+  //       setSolution(randomSolution.word)
+  //   })
+  // }, [setSolution])
+  
+  //
   const [solution , setSolution] = useState(null);
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
+    fetch('https://raw.githubusercontent.com/coffeeSR/wordle-json/main/db.json')
     .then(res => res.json())
     .then(json => {
       //random int between 0 and 24
-        const  randomSolution = json[Math.floor(Math.random()*json.length)]
+        const  randomSolution = json.solutions[Math.floor(Math.random()*json.solutions.length)]
         setSolution(randomSolution.word)
     })
   }, [setSolution])
+
+
   console.log({solution})
   return (
     <div className="App">
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,11 +3,21 @@ import React, { useEffect, useState } from 'react'
 export default function Keypad({usedKeys}) {
     const [letters, setLetters] = useState(null)
 
+    // useEffect(() => {
+    //     fetch('http://localhost:3001/letters')
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         setLetters(json)
+    //     })
+    // }, [])
+
+
+    //
     useEffect(() => {
-        fetch('http://localhost:3001/letters')
+        fetch('https://raw.githubusercontent.com/coffeeSR/wordle-json/main/db.json')
         .then(res => res.json())
         .then(json => {
-            setLetters(json)
+            setLetters(json.letters)
         })
     }, [])
 
